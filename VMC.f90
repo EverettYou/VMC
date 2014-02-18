27ba04e7ccb21b467af111783d8a8fcc1891673c
EverettYou <everettyou.internet@gmail.com>
2014-02-18 02:03:43 -0800

    Grid system
    set up grid system, complete and test MAKE_GRID, assigned to
    initialization.

--- VMC.f90
+++ VMC.f90
@@ -5,9 +5,46 @@ MODULE CONST
 END MODULE CONST
 ! ############### MODEL ###################
 MODULE MODEL
+	INTEGER :: L = 4 ! lattice size LxL, L should be even
 END MODULE MODEL
+! ############### GRID ####################
+MODULE GRID
+	INTEGER, ALLOCATABLE :: XS(:,:)
+	REAL,    ALLOCATABLE :: KS(:,:)
+CONTAINS
+! make grid
+SUBROUTINE MAKE_GRID()
+	USE CONST
+	USE MODEL
+	INTEGER :: N, I, I1, I2
+	
+	N = L**2/2 ! number of unit cells
+	ALLOCATE(XS(2*N,2), KS(N,2)) ! allocate x and k grid
+	! filling in x grid (x1 runs faster)
+	I = 0
+	DO I2 = 0, L-1
+	DO I1 = 0, L-1
+		I = I + 1 ! I inc
+		XS(I,1) = I1
+		XS(I,2) = I2
+	END DO !I1
+	END DO !I2
+	! filling in k grid (k1 runs faster)
+	I = 0
+	DO I2 = 0, L-1
+	DO I1 = 0, L/2-1
+		I = I + 1 ! I inc
+		KS(I,1) = I1
+		KS(I,2) = I2
+	END DO !I1
+	END DO !I2
+	KS = KS*(2*PI/L)
+END SUBROUTINE MAKE_GRID
+! end of module GRID
+END MODULE GRID
 ! ############# PHYSICS ###################
 MODULE PHYSICS
+	
 CONTAINS
 
 ! end of module PHYSICS
@@ -15,17 +52,35 @@ END MODULE PHYSICS
 ! ############### TASK ####################
 MODULE TASK
 CONTAINS
-! ----------- data -----------------
+! ----------- init ------------------
+! global initialization
+SUBROUTINE INIT()
+	USE GRID
+	
+	CALL MAKE_GRID() ! make grid
+END SUBROUTINE INIT
+! ----------- data ------------------
 ! ----------- tests -----------------
 ! test
 SUBROUTINE TEST()
 	
 END SUBROUTINE TEST
+! test grid
+SUBROUTINE TEST_GRID()
+	USE GRID
+	USE MATHIO
+	
+	CALL EXPORT('XS',XS)
+	CALL EXPORT('KS',KS)
+END SUBROUTINE TEST_GRID
 ! end of module task
 END MODULE TASK
 ! ############## PROGRAM ##################
 PROGRAM MAIN
 	USE TASK
 	PRINT *, '------------ VMC -------------'
+	CALL INIT()
 	
+!	CALL TEST()
+	CALL TEST_GRID()
 END PROGRAM MAIN
